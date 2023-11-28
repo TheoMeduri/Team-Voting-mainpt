079b94939b92206b2c8d7ac95169d42dd4601c3f
Theo Meduri <theomeduri@gmail.com>
2023-11-28 18:40:07 -0300

    Adicionando fire base

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyAwhqlLRRYRaSUn91BQRB4wvVoJ3jF-ZwI",
+        authDomain: "corrida-de-carros--multi-692a8.firebaseapp.com",
+        databaseURL: "https://corrida-de-carros--multi-692a8-default-rtdb.firebaseio.com",
+        projectId: "corrida-de-carros--multi-692a8",
+        storageBucket: "corrida-de-carros--multi-692a8.appspot.com",
+        messagingSenderId: "825564781105",
+        appId: "1:825564781105:web:40ff7ccb0f8f79c5f65310"
+      
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
